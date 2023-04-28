735524d60a910a3b55aba038622d5cc734527399
luisfelipe98 <luisfelipemm98@gmail.com>
2023-04-28 16:56:10 -0300

    Criação da Model de filmes

---
+++ interfaces/MovieDAOInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+interface MovieDAOInterface {
+
+    public function buildMovie($data);
+    public function findAll();
+    public function getLatestMovies();
+    public function getMoviesByCategory($category);
+    public function getMoviesByUsersId($id);
+    public function findById($id);
+    public function findByTitle($title);
+    public function create(Movie $movie);
+    public function update(Movie $movie);
+    public function destroy($id);
+    
+}
+
+?>
---
+++ models/Movie.php
@@ -0,0 +1,84 @@
+<?php
+
+class Movie {
+
+    private $id;
+    private $title;
+    private $description;
+    private $image;
+    private $trailer;
+    private $category;
+    private $length;
+    private $users_id;
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getId() {
+        return $this->id;    
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function getTitle() {
+        return $this->title;    
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function getDescription() {
+        return $this->description;    
+    }
+
+    public function setImage($image) {
+        $this->image = $image;
+    }
+
+    public function getImage() {
+        return $this->image;    
+    }
+
+    public function setTrailer($trailer) {
+        $this->trailer = $trailer;
+    }
+
+    public function getTrailer() {
+        return $this->trailer;    
+    }
+
+    public function setCategory($category) {
+        $this->category = $category;
+    }
+
+    public function getCategory() {
+        return $this->category;    
+    }
+
+    public function setLength($length) {
+        $this->length = $length;
+    }
+
+    public function getLength() {
+        return $this->length;    
+    }
+
+    public function setUsersId($users_id) {
+        $this->users_id = $users_id;
+    }
+
+    public function getUsersId() {
+        return $this->users_id;    
+    }
+
+    public function generateImageName() {
+        return bin2hex(random_bytes(60)) . ".jpg";
+    }
+
+}
+
+?>
